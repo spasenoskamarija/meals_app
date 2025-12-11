4153a83f9e44b1cf794f0db3e73e3f4344a22330
Marija <marija.spasenoska@students.finki.ukim.mk>
2025-12-11 20:22:59 +0100

    edited meals_page.dart

--- lib/screens/meals_page.dart
+++ lib/screens/meals_page.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import '../data/api_data.dart';
+import '../data/favorite_service.dart';
+import '../models/favorite_model.dart';
 import '../models/meal.dart';
 import '../widgets/meal_card.dart';
+import 'favorites_page.dart';
 import 'meal_detail_page.dart';
-
+//
 class MealsPage extends StatefulWidget {
   final String category;
   const MealsPage({required this.category, super.key});
@@ -15,11 +18,20 @@ class MealsPage extends StatefulWidget {
 class _MealsPageState extends State<MealsPage> {
   List<Meal> meals = [];
   List<Meal> filtered = [];
+  Set<String> favoriteIds = {};
+
+  final FavoriteService favoriteService = FavoriteService();
 
   @override
   void initState() {
     super.initState();
     load();
+
+    favoriteService.getFavorites().listen((favList) {
+      setState(() {
+        favoriteIds = favList.map((f) => f.mealId).toSet();
+      });
+    });
   }
 
   void load() async {
@@ -35,6 +47,30 @@ class _MealsPageState extends State<MealsPage> {
       filtered = await ApiService.searchMeals(query);
     }
     setState(() {});
+  }
+
+  void toggleFavorite(String mealId) async {
+    final meal = meals.firstWhere((m) => m.id == mealId);
+
+    setState(() {
+      if (favoriteIds.contains(mealId)) {
+        favoriteIds.remove(mealId);
+      } else {
+        favoriteIds.add(mealId);
+      }
+    });
+
+    final favorite = Favorite(
+      mealId: meal.id,
+      name: meal.name,
+      imageUrl: meal.img,
+    );
+
+    if (favoriteIds.contains(mealId)) {
+      await favoriteService.addFavorite(favorite);
+    } else {
+      await favoriteService.removeFavorite(mealId);
+    }
   }
 
   @override
@@ -56,14 +92,16 @@ class _MealsPageState extends State<MealsPage> {
           Expanded(
             child: GridView.builder(
               padding: EdgeInsets.all(10),
-              gridDelegate:
-              SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2),
               itemCount: filtered.length,
               itemBuilder: (context, i) {
+                final meal = filtered[i];
                 return MealCard(
-                  meal: filtered[i],
+                  meal: meal,
+                  isFavorite: favoriteIds.contains(meal.id),
                   onTap: () async {
-                    final detail = await ApiService.getMealDetail(filtered[i].id);
+                    final detail = await ApiService.getMealDetail(meal.id);
                     Navigator.push(
                       context,
                       MaterialPageRoute(
@@ -71,11 +109,28 @@ class _MealsPageState extends State<MealsPage> {
                       ),
                     );
                   },
+                  onFavoriteToggle: () => toggleFavorite(meal.id),
                 );
               },
             ),
           ),
         ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.favorite),
+        onPressed: () {
+          final favorites =
+          meals.where((m) => favoriteIds.contains(m.id)).toList();
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (_) => FavoritesPage(
+                favoriteMeals: favorites,
+                onFavoriteToggle: toggleFavorite,
+              ),
+            ),
+          );
+        },
       ),
     );
   }
